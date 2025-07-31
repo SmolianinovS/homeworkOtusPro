9a136177080beb8f10ef4b4ac496ebb49ebf61d7
lsdvitamin <lsdvitamin7@gmail.com>
2025-07-31 11:00:24 +0300

    Внес изменения в ДЗ_02 по замечаниям преподавателя

--- HW02-generics/src/main/java/homework/CustomerService.java
+++ HW02-generics/src/main/java/homework/CustomerService.java
@@ -2,10 +2,11 @@ package homework;
 
 import java.util.Comparator;
 import java.util.Map;
+import java.util.NavigableMap;
 import java.util.TreeMap;
 
 public class CustomerService {
-    TreeMap<Customer, String> customers = new TreeMap<>(Comparator.comparing(Customer::getScores));
+    private final NavigableMap<Customer, String> customers = new TreeMap<>(Comparator.comparing(Customer::getScores));
 
     public Map.Entry<Customer, String> getSmallest() {
         Map.Entry<Customer, String> customerStringEntry = customers.firstEntry();
